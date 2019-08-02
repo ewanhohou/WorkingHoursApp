23c0ace2b4bcf3c4ba0f33395ba96eecc9cd1fb7
yuju <yuju@1608020NB01.local>
2019-08-02 20:19:47 +1000

    frontend 電話驗證

--- frontend/src/components/validations/inputTelValid.vue
+++ frontend/src/components/validations/inputTelValid.vue
@@ -1,14 +1,44 @@
 <template>
 <inputTemplate :label="label">
-    <input :value="value" type="tel" class="form-control" :id="id" :placeholder="id" required @change="$emit('input', $event.target.value)">
+    <input-mask v-model="tel" mask="0499999999" :id="id" :placeholder="id" class="form-control"></input-mask>
+    <i v-show="errorShow" class="fa fa-warning text-red"> {{label}}格式錯誤 (例：0422791640)</i>
 </inputTemplate>
 </template>
 
 <script>
 import inputTemplate from '@/components/template/inputTemplate'
+import InputMask from 'vue-input-mask';
 
 export default {
     name: 'inputTelValid',
+    data() {
+        return {
+            tel: '',
+            errorShow: false
+        }
+    },
+    watch: {
+        tel(val) {
+            if (this.regex()) this.errorShow = false;
+            this.$emit('input', val);
+        },
+        value(val) {
+            this.tel = val;
+        }
+    },
+    methods: {
+        require() {
+            if (this.regex()) {
+                this.errorShow = false;
+                return true;
+            }
+            this.errorShow = true;
+            return false;
+        },
+        regex() {
+            return this.tel.match(/^04\d{8}$/);
+        }
+    },
     props: {
         id: String,
         label: String,
@@ -16,6 +46,7 @@ export default {
     },
     components: {
         inputTemplate,
-    }
+        InputMask
+    },
 }
 </script>

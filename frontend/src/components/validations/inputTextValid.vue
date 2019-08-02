8cd64a3f322cf05843e965802937c83a8de80548
yuju <yuju@1608020NB01.local>
2019-08-02 20:20:04 +1000

    frontend 必填驗證

--- frontend/src/components/validations/inputTextValid.vue
+++ frontend/src/components/validations/inputTextValid.vue
@@ -1,6 +1,7 @@
 <template>
 <inputTemplate :label="label">
-    <input :value="value" type="text" class="form-control" :id="id" :placeholder="id" autofocus required @change="$emit('input', $event.target.value)">
+    <input v-model="text" type="text" class="form-control is-invalid" :id="id" :placeholder="id" autofocus @change="change">
+    <i v-show="errorShow" class="fa fa-warning text-red"> {{label}}必填</i>
 </inputTemplate>
 </template>
 
@@ -9,10 +10,35 @@ import inputTemplate from '@/components/template/inputTemplate'
 
 export default {
     name: 'inputTextValid',
+    data() {
+        return {
+            text: '',
+            errorShow: false
+        }
+    },
+    watch: {
+        value(val) {
+            this.text = val;
+        }
+    },
     props: {
         id: String,
         label: String,
         value: String
+    },
+    methods: {
+        require() {
+            if (this.text == '') {
+                this.errorShow = true;
+                return false;
+            }
+            this.errorShow = false;
+            return true;
+        },
+        change($event) {
+            this.require();
+            this.$emit('input', $event.target.value);
+        }
     },
     components: {
         inputTemplate,

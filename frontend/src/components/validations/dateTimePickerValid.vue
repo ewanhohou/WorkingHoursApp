da06af7760509a1733c62c494ce3c3517304e024
yuju <yuju@1608020NB01.local>
2019-08-02 21:20:04 +1000

    frontend 日期驗證

--- frontend/src/components/validations/dateTimePickerValid.vue
+++ frontend/src/components/validations/dateTimePickerValid.vue
@@ -1,7 +1,14 @@
 <template>
-<inputTemplate :label="label">
-    <Datetime v-model="dt" type="datetime" format="dd-MM-yyyy HH:mm" :minute-step="5" use12-hour input-class="form-control" class="theme-blue"></Datetime>
-</inputTemplate>
+<div>
+    <inputTemplate label="起始日期">
+        <Datetime v-model="sdt" type="datetime" format="dd-MM-yyyy HH:mm" :minute-step="5" use12-hour input-class="form-control" class="theme-blue"></Datetime>
+        <i v-show="sdtErrorShow" class="fa fa-warning text-red"> 起始日期必填</i>
+    </inputTemplate>
+    <inputTemplate label="結束日期">
+        <Datetime v-model="edt" type="datetime" format="dd-MM-yyyy HH:mm" :minute-step="5" :min-datetime="sdt" use12-hour input-class="form-control" class="theme-blue"></Datetime>
+        <i v-show="edtErrorShow" class="fa fa-warning text-red"> 結束日期必填</i>
+    </inputTemplate>
+</div>
 </template>
 
 <script>
@@ -13,12 +20,15 @@ import "vue-datetime/dist/vue-datetime.css";
 export default {
     name: 'dateTimePickerValidValid',
     props: {
-        label: String,
-        value: String
+        startTimeOld: String,
+        endTimeOld: String,
     },
     data() {
         return {
-            dt: '',
+            sdt: '',
+            edt: '',
+            sdtErrorShow: false,
+            edtErrorShow: false
         };
     },
     components: {
@@ -26,13 +36,33 @@ export default {
         Datetime
     },
     watch: {
-        dt($event) {
-            this.$emit('dateTimeChangEvent', $event);
+        sdt($event) {
+            if ($event !== '') this.sdtErrorShow = false;
+            this.$emit('setStartTime', $event);
         },
-        value(val) {
-            this.dt = val;
+        edt($event) {
+            if ($event == '') this.edtErrorShow = false;
+            this.$emit('setEndTime', $event);
+        },
+        startTimeOld(val) {
+            this.sdt = val;
+        },
+        endTimeOld(val) {
+            this.edt = val;
         }
-    }
+    },
+    methods: {
+        require() {
+            if (this.sdt == '' || this.edt == '') {
+                if (this.sdt == '') this.sdtErrorShow = true;
+                if (this.edt == '') this.edtErrorShow = true;
+                return false;
+            }
+            this.sdtErrorShow = false;
+            this.edtErrorShow = false;
+            return true;
+        },
+    },
 }
 </script>
 

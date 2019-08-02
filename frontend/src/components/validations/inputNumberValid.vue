a8e7d795887f0b71b9f5352c6516534a56a5b9ae
yuju <yuju@1608020NB01.local>
2019-08-02 20:37:32 +1000

    frontend  數字驗證

--- frontend/src/components/validations/inputNumberValid.vue
+++ frontend/src/components/validations/inputNumberValid.vue
@@ -1,6 +1,8 @@
 <template>
 <inputTemplate :label="label">
-    <input :value="value" type="number" class="form-control" :id="id" :placeholder="id" required @change="$emit('input', parseInt($event.target.value))">
+    <input v-model="number" type="number" class="form-control" :id="id" :placeholder="id">
+    <i v-show="errorShow" class="fa fa-warning text-red"> {{label}}請輸入數字</i>
+
 </inputTemplate>
 </template>
 
@@ -9,13 +11,41 @@ import inputTemplate from '@/components/template/inputTemplate'
 
 export default {
     name: 'inputNumberValid',
+    data() {
+        return {
+            number: 0,
+            errorShow: false
+        }
+    },
     props: {
         id: String,
         label: String,
         value: Number
     },
+    watch: {
+        number(num) {
+            if (this.regex()) this.errorShow = false;
+            this.$emit('input', parseInt(num));
+        },
+        value(val) {
+            this.number = val;
+        }
+    },
     components: {
         inputTemplate,
+    },
+    methods: {
+        require() {
+            if (this.regex()) {
+                this.errorShow = false;
+                return true;
+            }
+            this.errorShow = true;
+            return false;
+        },
+        regex() {
+            return this.number.toString().match("^[0-9]*$");
+        }
     },
 }
 </script>
